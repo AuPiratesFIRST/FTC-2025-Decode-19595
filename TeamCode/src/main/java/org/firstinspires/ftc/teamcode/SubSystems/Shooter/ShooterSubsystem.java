f8d179cac650037c999979fc98c65ac7fe3c4cd4
chriswn <onichr1074@apps.district279.org>
2025-12-08 22:50:02 -0600

    getter for targetRPM in ShooterSubsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Shooter/ShooterSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Shooter/ShooterSubsystem.java
@@ -171,6 +171,10 @@ public class ShooterSubsystem {
         return (leftRPM + rightRPM) / 2.0;
     }
 
+    public double getTargetRPM() {
+        return targetRPM;
+    }
+
     public void stop() {
         leftShooterMotor.setVelocity(0);
         rightShooterMotor.setVelocity(0);
